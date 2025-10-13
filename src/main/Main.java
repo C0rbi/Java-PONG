fd12aece06bc8a0020614510592fcf058741dd44
C0rbi <allan.nicolas331966@gmail.com>
2025-10-13 12:05:53 +1100

    Changement du nom de la fenêtre

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // permet de quite la fenêtre
         window.setResizable(false); // permet de ne pas pouvoir modifier la taille de la fenêtre
-        window.setTitle("2D Game"); // donne un nom à la fenêtre
+        window.setTitle("Java-PONG"); // donne un nom à la fenêtre
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel); // normalement c juste pour applique la class GamePanel a la fenêtre.
